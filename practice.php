2f7a217acaca24c61d36290748faf072dbe48987
yoyomesi <54796124+hiroki8ir0ki@users.noreply.github.com>
2021-08-11 16:30:25 +0900

    php de

---
+++ practice.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        
+$count = 1;   //変数の定義
+
+while($count <= 4){   
+  echo $count *2 ; 
+  echo '<br>';  
+  $count++;  
+}
+    ?>
+</body>
+</html>
